1b54cc5c8d6eb8fbd97c3af98ab41b2e6835db94
John Settineri <settineri.john@gmail.com>
2019-11-08 21:10:57 -0500

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Home from './Component/Home.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <Home></Home>
       </header>
     </div>
   );
